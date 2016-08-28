2062250abf7ad730fe8771d13e839a10297b916f
Miso Zmiric (Mike Squinter) <miso.zmiric@gmail.com>
2016-08-28 23:13:59 +0200

    add redux dep and add navigation template to the side bar

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import './SideBar.css';
 import logo from './logo.png';
 
@@ -9,6 +10,13 @@ class SideBar extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>OpenRCT2</h2>
+        </div>
+        <div className="Navigation">
+          <ul role="nav">
+            <li><Link to="/servers">Servers</Link></li>
+            <li><Link to="/objects">Objects</Link></li>
+            <li><Link to="/packs">Packs</Link></li>
+          </ul>
         </div>
       </div>
     );
