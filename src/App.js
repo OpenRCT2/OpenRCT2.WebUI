531e194d5331973a748b520145ca866fd6ca16ea
Miso Zmiric (Mike Squinter) <miso.zmiric@gmail.com>
2016-08-28 21:22:31 +0200

    add create-react-app scafolding

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to OpenRCT2 WebUI</h2>
+        </div>
+        <p className="App-intro">
+          Coming soon.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
