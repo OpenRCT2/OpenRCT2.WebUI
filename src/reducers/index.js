fa4c83f42ad0c36d90edee7670f91fe96be923de
Miso Zmiric (Mike Squinter) <miso.zmiric@gmail.com>
2016-08-29 08:32:06 +0200

    add reducers for servers and objects

---
+++ src/reducers/index.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux';
+
+import createServerList, * as fromServerList from './servers'; // eslint-disable-next-line
+import createObjectList, * as fromObjectList from './objects';
+
+const servers = createServerList()
+const objects = createObjectList()
+
+const orct = combineReducers({
+  servers,
+  objects,
+});
+
+export default orct;
+
+export const getServers = (state) => {
+  const ids = fromServerList.getServers(state.servers);
+  return ids;
+};
+
+export const getIsFetching = (state) =>
+  fromServerList.getIsFetching(state.servers);
+
+export const getErrorMessage = (state) =>
+  fromServerList.getErrorMessage(state.servers);
---
+++ src/reducers/servers.js
@@ -0,0 +1,48 @@
+import { combineReducers } from 'redux';
+
+const createServerList = () => {
+ const servers = (state = [], action) => {
+   switch (action.type) {
+     case 'FETCH_SERVERS_SUCCESS':
+       return action.response.result
+     default:
+       return state;
+   }
+ };
+
+ const isFetching = (state = false, action) => {
+   switch (action.type) {
+     case 'FETCH_SERVERS_REQUEST':
+       return true;
+     case 'FETCH_SERVERS_SUCCESS':
+     case 'FETCH_SERVERS_FAILURE':
+       return false;
+     default:
+       return state;
+   }
+ };
+
+ const errorMessage = (state = null, action) => {
+   switch (action.type) {
+     case 'FETCH_SERVERS_FAILURE':
+       return action.message;
+     case 'FETCH_SERVERS_REQUEST':
+     case 'FETCH_SERVERS_SUCCESS':
+       return null;
+     default:
+       return state;
+   }
+ };
+
+ return combineReducers({
+   servers,
+   isFetching,
+   errorMessage,
+ });
+};
+
+export default createServerList;
+
+export const getServers = (state) => state.servers;
+export const getIsFetching = (state) => state.isFetching;
+export const getErrorMessage = (state) => state.errorMessage;
