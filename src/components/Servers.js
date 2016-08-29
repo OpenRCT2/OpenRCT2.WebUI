2719aff6010b1a90ecb2fe67fe01a85c3fe04407
Miso Zmiric (Mike Squinter) <miso.zmiric@gmail.com>
2016-08-29 08:36:28 +0200

    Move the rest of the components to the new directory structure and use redux

---
+++ src/components/Servers.js
@@ -0,0 +1,73 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import * as actions from '../actions';
+import { getServers, getErrorMessage, getIsFetching } from '../reducers';
+import FetchError from './FetchError';
+
+class Servers extends Component {
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    const { fetchServers } = this.props;
+    fetchServers();
+  }
+
+  render() {
+    const { isFetching, errorMessage, servers } = this.props;
+    if (isFetching && !servers.length) {
+      return (
+        <div className="Servers">
+          <h1>Servers</h1>
+          <p className="Servers-intro">
+            Loading...
+          </p>
+        </div>
+      );
+    }
+    if (errorMessage && !servers.length) {
+      return (
+        <div className="Servers">
+          <h1>Servers</h1>
+          <FetchError
+            message={errorMessage}
+            onRetry={() => this.fetchData()}
+          />
+        </div>
+      );
+    }
+    return (
+      <div className="Servers">
+        <h1>Servers</h1>
+        <p className="Servers-intro">
+          Servers list.
+          {servers}
+        </p>
+      </div>
+    );
+  }
+}
+
+Servers.propTypes = {
+  errorMessage: PropTypes.string,
+  servers: PropTypes.array.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  fetchServers: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state, { params }) => {
+  return {
+    isFetching: getIsFetching(state),
+    errorMessage: getErrorMessage(state),
+    servers: getServers(state),
+  };
+};
+
+Servers = withRouter(connect(
+  mapStateToProps,
+  actions
+)(Servers));
+
+export default Servers;
