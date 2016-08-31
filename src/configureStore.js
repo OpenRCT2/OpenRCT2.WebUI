880512b73edd19ef3565b1bd8e2bcd4d12463f00
Miso Zmiric (Mike Squinter) <miso.zmiric@gmail.com>
2016-08-31 06:55:42 +0200

    add localStorage utils and make configureStore use it
    we should probably only actually do the saveState on login and logout,
    but i was lazy and rushed this

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import orctApp from './reducers';
+import throttle from 'lodash/throttle';
+import { loadState, saveState } from './utils/localStorage';
 
 const configureStore = () => {
   const middleware = [thunk];
@@ -9,10 +11,21 @@ const configureStore = () => {
     middleware.push(createLogger());
   }
 
-  return createStore(
+  const persistedState = loadState();
+
+  const store = createStore(
     orctApp,
+    persistedState,
     applyMiddleware(...middleware)
   );
+
+  store.subscribe(throttle(() => {
+    saveState({
+      auth: store.getState().auth,
+    });
+  }, 1000));
+
+  return store;
 };
 
 export default configureStore;
