def2eafb867a82b72f0e912e828ad2754c812c4d
Ted John <ted@brambles.org>
2018-02-26 18:36:04 +0000

    Fix news reducer

--- src/reducers/news.js
+++ src/reducers/news.js
@@ -1,25 +1,32 @@
+const defaultState = {
+  items: [],
+  hasFetched: false,
+  isFetching: false
+};
+
 const initialState = (() => {
   const news = localStorage['news'];
   if (news) {
     try {
-      return JSON.parse(news);
+      const saved = JSON.parse(news);
+      if (Array.isArray(saved.items)) {
+        return { ...defaultState, items: saved.items };
+      }
     } catch (e) { }
   }
-  return {
-    items: []
-  };
+  return defaultState;
 })();
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_NEWS_REQUEST':
-      return { isFetching: true, ...state };
+      return { ...state, isFetching: true };
     case 'FETCH_NEWS_SUCCESS':
       const items = action.response;
       localStorage['news'] = JSON.stringify({ items });
-      return { hasFetched: true, items };
+      return { hasFetched: true, isFetching: false, items };
     case 'FETCH_NEWS_FAILURE':
-      return { isFetching: false, ...state };
+      return { ...state, isFetching: false };
     default:
       return state;
   }
